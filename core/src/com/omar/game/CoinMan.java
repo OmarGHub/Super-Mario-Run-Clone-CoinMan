04e3c47862ee31de75d283677f9b7b2a81221bc6
Omar <sx40hs@gmail.com>
2020-05-31 19:14:06 +0300

    6. Jumping

--- core/src/com/omar/game/CoinMan.java
+++ core/src/com/omar/game/CoinMan.java
@@ -12,6 +12,10 @@ public class CoinMan extends ApplicationAdapter {
 	Texture[] man;
 	int manStat = 0;
 	int pause = 0;
+	float gravity = 0.2f;
+	float velocity = 0;
+	int manY = 0;
+
 
 	@Override
 	public void create () {
@@ -22,6 +26,7 @@ public class CoinMan extends ApplicationAdapter {
 		man[1] = new Texture("frame-2.png");
 		man[2] = new Texture("frame-3.png");
 		man[3] = new Texture("frame-3.png");
+		manY = Gdx.graphics.getHeight() / 2;
 
 	}
 
@@ -29,6 +34,11 @@ public class CoinMan extends ApplicationAdapter {
 	public void render () {
 		batch.begin();
 		batch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+
+
+		if (Gdx.input.justTouched()){
+			velocity = -10;
+		}
 
 		if (pause < 8){
 			pause++;
@@ -41,10 +51,14 @@ public class CoinMan extends ApplicationAdapter {
 			}
 
 		}
+		velocity += gravity;
+		manY -= velocity;
 
+		if (manY <= 0) {
+			manY = 0;
+		}
 
-
-		batch.draw(man[manStat], Gdx.graphics.getWidth()/2 - man[manStat].getWidth()/2, Gdx.graphics.getHeight()/2);
+		batch.draw(man[manStat], Gdx.graphics.getWidth()/2 - man[manStat].getWidth()/2, manY);
 
 
 		batch.end();
